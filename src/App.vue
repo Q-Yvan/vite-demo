ed70e0646fd04627bf5b1c8ae00375bcc2043566
quyifeng <912456041@qq.com>
2021-11-18 20:52:25 +0800

    增加ts和简单todolist

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <Layout />
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Layout from './components/Layout.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+@Options({
+  components: {
+    // HelloWorld,
+    Layout
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style>
 #app {
---
+++ src/components/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input v-model="obj.name" type="text">
+    <button @click="handleAddName">+</button>
+  </div>
+  <div>
+    列表：
+    <p v-for="(item, index) in obj.nameList" :key="item" @click="handleDelete(index)">{{ item }}</p>
+  </div>
+  <div>数量：{{ count }}<button @click="handleAdd">+</button></div>
+</template>
+<script>
+import { defineComponent, reactive, ref } from 'vue'
+
+export default defineComponent({
+  setup(props, context) {
+    const count = ref(0)
+    const handleAdd = () => {
+      count.value++
+    }
+    const obj = reactive({
+      name: '',
+      nameList: []
+    })
+    const handleAddName = () => {
+      obj.nameList.push(obj.name)
+    }
+    const handleDelete = (index) => {
+      obj.nameList.splice(index, 1)
+    }
+    return { count, handleAdd, obj, handleAddName, handleDelete }
+  },
+  render() {}
+})
+</script>
